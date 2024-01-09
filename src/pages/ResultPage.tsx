f05977a2a2407c37317bd0842b9777e87c29ae95
AndikanAffiah <andikanaffiah@gmail.com>
2024-01-09 17:30:15 +0100

    revert alignment

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -39,14 +39,7 @@ export default function ResultPage() {
         <div className={"relative"}>
             <div
                 className={"sticky top-0 bg-emerald-100 py-10 px-16 flex flex-col md:flex-row items-center gap-6 z-20"}>
-                <div>
-                    <Link to={"/create/new"}>
-                        <button
-                            className={"border border-black px-6 py-4 rounded-xl hover:text-white hover:bg-black transition-all duration-300"}>
-                            Upload Article
-                        </button>
-                    </Link>
-                </div>
+
                 <div className={"flex justify-center items-center grow"}>
                     <input
                         onChange={(e) => setSearchValue(e.target.value)}
@@ -60,8 +53,14 @@ export default function ResultPage() {
                              className={"transition-all hover:animate-[wiggle_1s_ease-in-out_infinite]"}/>
                     </div>
                 </div>
-
-
+                <div>
+                    <Link to={"/create/new"}>
+                        <button
+                            className={"border border-black px-6 py-4 rounded-xl hover:text-white hover:bg-black transition-all duration-300"}>
+                            Upload Article
+                        </button>
+                    </Link>
+                </div>
             </div>
             <div className={"relative px-16"}>
                 <div className={"text-5xl my-6 bg-white py-6 sticky top-36"}>Results:</div>
